b43c106e51ce9bd8cd48128483161defe869bcb2
hamza khan <hamzakhan.4u99@gmail.com>
2025-10-24 20:16:34 +0500

    code updated

--- modules/community/community.controller.js
+++ modules/community/community.controller.js
@@ -203,7 +203,7 @@ exports.createPost = async (req, res) => {
 				`${currentUser.firstName} ${currentUser.lastName} posted: ${title}`,
 				"community_announcement",
 				{
-					post: JSON.stringify(post),
+					post: JSON.stringify(post.toJSON()),
 					category: "Announcements"
 				}
 			);
@@ -214,7 +214,7 @@ exports.createPost = async (req, res) => {
 				`${currentUser.firstName} ${currentUser.lastName} posted in ${category.title}`,
 				"community_post",
 				{
-					post: JSON.stringify(post),
+					post: JSON.stringify(post.toJSON()),
 					category: category.title
 				}
 			);
